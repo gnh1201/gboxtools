69fab3704dda0a15307496d670c430607295cf75
Namhyeon, Go <gnh1201@gmail.com>
2017-08-14 01:51:05 +0900

    commit 2

---
+++ extend/boxtools.extend.php
@@ -0,0 +1,9 @@
+<?php
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+/*** Boxtools ***/
+/*** Go Namhyeon ***/
+
+// Boxtools Autoload 연동
+include_once(G5_PLUGIN_PATH . "/boxtools/vendor/autoload.php");
+?>
